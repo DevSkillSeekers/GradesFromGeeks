851ce8d493228d19c23846b3ee13d3e1c6192a2f
Nada Feteiha <nada.feteiha@gmail.com>
2024-01-24 16:08:45 -0800

    add api

---
+++ app/src/main/java/com/solutionteam/mindfulmentor/data/network/response/SignInResult.kt
@@ -0,0 +1,12 @@
+package com.solutionteam.mindfulmentor.data.network.response
+
+data class SignInResult(
+    val data: UserData?,
+    val errorMessage: String?
+)
+
+data class UserData(
+    val userId: String,
+    val username: String?,
+    val profilePictureUrl: String?
+)
