f8ae37ef503a37817657606a322a424bc7571ad9
Nada Feteiha <nada.feteiha@gmail.com>
2024-01-25 22:35:43 -0800

    add mentor module

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,4 +8,5 @@ buildscript {
 plugins {
     id("com.android.application") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.library") version "8.2.1" apply false
 }
