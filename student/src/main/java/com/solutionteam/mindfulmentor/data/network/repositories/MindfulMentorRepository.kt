a0f137f7d0fa01caca1510b594c480781ba50e72
Nada Feteiha <nada.feteiha@gmail.com>
2024-01-27 00:22:38 -0800

    add getMentors fake data

--- student/src/main/java/com/solutionteam/mindfulmentor/data/network/repositories/MindfulMentorRepository.kt
+++ student/src/main/java/com/solutionteam/mindfulmentor/data/network/repositories/MindfulMentorRepository.kt
@@ -1,4 +1,12 @@
 package com.solutionteam.mindfulmentor.data.network.repositories
 
+import com.solutionteam.mindfulmentor.data.entity.Mentor
+
 interface MindfulMentorRepository {
+
+    //region Mentor
+    suspend fun getMentors(): List<Mentor>
+
+    //endregion
+
 }
--- student/src/main/java/com/solutionteam/mindfulmentor/data/network/repositories/MindfulMentorRepositoryImp.kt
+++ student/src/main/java/com/solutionteam/mindfulmentor/data/network/repositories/MindfulMentorRepositoryImp.kt
@@ -1,9 +1,47 @@
 package com.solutionteam.mindfulmentor.data.network.repositories
 
+import com.solutionteam.mindfulmentor.data.entity.Mentor
 import com.solutionteam.mindfulmentor.data.local.database.MindfulMentorDao
 import com.solutionteam.mindfulmentor.data.network.BaseRepository
 
 class MindfulMentorRepositoryImp(
     private val mindfulMentorDao: MindfulMentorDao
-): BaseRepository(),MindfulMentorRepository {
+) : BaseRepository(), MindfulMentorRepository {
+
+    override suspend fun getMentors(): List<Mentor> {
+        return generatorMentor()
+    }
+
+
+    //region Fake Data
+
+    private fun generatorMentor(): List<Mentor> {
+        val list = mutableListOf<Mentor>()
+        for (i in 0..10) {
+            list.add(
+                Mentor(
+                    id = "$i",
+                    name = "First Last$i",
+                    imageUrl = getImage(),
+                    rate = (0..10).random().toDouble(),
+                    numberReviewers = (1..500).random(),
+                )
+            )
+        }
+        return list
+    }
+
+
+    private fun getImage(): String {
+        val list = listOf(
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGuH6Vo5XDGGvgriYJwqI9I8efWEOeVQrVTw&usqp=CAU",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_p4wGt_hng5BeADmgd6lf0wPrY6aOssc3RA&usqp=CAU",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFiWs0Sx8Omxw_qamwrZT_EYTz27HulwjRBA&usqp=CAU",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo5xoN3QF2DBxrVUq7FSxymtDoD3-_IW5CgQ&usqp=CAU"
+        )
+
+        return list.shuffled().first()
+    }
+
+    //endregion
 }
