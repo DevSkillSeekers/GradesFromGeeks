72e61f9b2ca7eb0eb99341062589cf436a41c8a8
Nada Feteiha <nada.feteiha@gmail.com>
2024-01-29 11:28:19 -0800

    add meeting

---
+++ student/src/main/java/com/solutionteam/mindfulmentor/ui/presentation/home/component/InComingMeeting.kt
@@ -0,0 +1,69 @@
+package com.solutionteam.mindfulmentor.ui.presentation.home.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.solutionteam.design_system.components.GGButton
+import com.solutionteam.design_system.components.GGTextChipStyle
+import com.solutionteam.design_system.theme.Theme
+import com.solutionteam.mindfulmentor.ui.presentation.home.MeetingUiState
+import com.solutionteam.mindfulmentor.utils.formatHoursMinutes
+import com.solutionteam.mindfulmentor.utils.formatTimestamp
+
+@Composable
+fun InComingMeeting(
+    meeting: MeetingUiState,
+    modifier: Modifier = Modifier,
+    onJoinClicked: () -> Unit
+) {
+    Column(
+        modifier = modifier
+            .background(Theme.colors.card, shape = RoundedCornerShape(20.dp))
+            .padding(vertical = 8.dp, horizontal = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+        horizontalAlignment = Alignment.Start
+    ) {
+
+        Text(
+            text = meeting.subject,
+            style = Theme.typography.titleSmall,
+            color = Theme.colors.primaryShadesDark
+        )
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            GGTextChipStyle(meeting.time.formatTimestamp())
+            GGTextChipStyle(meeting.mentorName)
+        }
+        Text(
+            text = meeting.notes,
+            maxLines = 2,
+            style = Theme.typography.labelLarge
+        )
+
+        GGButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 4.dp),
+            title = if (meeting.enableJoin) {
+                "Join now"
+            } else {
+                "Join after..(${meeting.reminder.formatHoursMinutes()})"
+            },
+            enabled = meeting.enableJoin,
+            onClick = onJoinClicked
+        )
+    }
+}
