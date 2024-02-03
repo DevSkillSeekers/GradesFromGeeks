fa0cedd35332b8afb65b8cf2444e59cd61f8dcc4
Asia sama <sehunexo710@gmail.com>
2024-02-03 14:25:45 +0300

    add data store preferences module

---
+++ student/src/main/java/com/solutionteam/mindfulmentor/di/DataStoreModule.kt
@@ -0,0 +1,24 @@
+package com.solutionteam.mindfulmentor.di
+
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.preferencesDataStoreFile
+import com.solutionteam.mindfulmentor.data.local.UserDataStorePreferencesImp
+import com.solutionteam.mindfulmentor.data.local.UserPreferences
+import org.koin.android.ext.koin.androidApplication
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.module
+
+val DataStoreModule = module {
+
+    single {
+        PreferenceDataStoreFactory.create(
+                produceFile = { androidApplication().preferencesDataStoreFile(
+                        UserDataStorePreferencesImp.PREFERENCES_FILE_NAME
+                ) }
+        )
+
+    }
+
+    singleOf(::UserDataStorePreferencesImp) { bind<UserPreferences>() }
+}
