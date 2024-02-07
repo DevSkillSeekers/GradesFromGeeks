9d7683b1a31ab7cb7285e0174ea208c1ebf5b0f8
yassenRamadan1 <118762446+yassenRamadan1@users.noreply.github.com>
2024-02-08 01:20:29 +0200

    refactor signup function in repository

--- student/src/main/java/com/solutionteam/mindfulmentor/data/network/repositories/AuthRepository.kt
+++ student/src/main/java/com/solutionteam/mindfulmentor/data/network/repositories/AuthRepository.kt
@@ -1,9 +1,8 @@
 package com.solutionteam.mindfulmentor.data.network.repositories
 
-import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 
 interface AuthRepository {
-    suspend fun signUp(email: String, password: String): Task<AuthResult>
+    suspend fun signUp(email: String, password: String): AuthResult
     suspend fun signIn(email: String, password: String): AuthResult
 }
--- student/src/main/java/com/solutionteam/mindfulmentor/data/network/repositories/AuthRepositoryImpl.kt
+++ student/src/main/java/com/solutionteam/mindfulmentor/data/network/repositories/AuthRepositoryImpl.kt
@@ -1,21 +1,24 @@
 package com.solutionteam.mindfulmentor.data.network.repositories
 
-import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 
 class AuthRepositoryImpl : AuthRepository {
     private val auth = Firebase.auth
-    override suspend fun signUp(email: String, password: String): Task<AuthResult> {
-        return auth.createUserWithEmailAndPassword(email, password)
+    override suspend fun signUp(email: String, password: String): AuthResult = suspendCancellableCoroutine { continuation ->
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    continuation.resume(task.result!!)
+                } else {
+                    continuation.resumeWithException(task.exception ?: Exception("Unknown registration error"))
+                }
+            }
     }
-
-    @OptIn(ExperimentalCoroutinesApi::class)
     override suspend fun signIn(email: String, password: String): AuthResult = suspendCancellableCoroutine { continuation ->
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
