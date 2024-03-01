9dda20153c092c5c7f34fe4c8e86d1d88ea3655f
Asia sama <sehunexo710@gmail.com>
2024-03-01 06:16:43 +0300

    implement logout logic

--- student/src/main/java/com/solutionteam/mindfulmentor/ui/profile/ProfileUIEffect.kt
+++ student/src/main/java/com/solutionteam/mindfulmentor/ui/profile/ProfileUIEffect.kt
@@ -2,4 +2,6 @@ package com.solutionteam.mindfulmentor.ui.profile
 
 sealed interface ProfileUIEffect {
     data object ProfileError : ProfileUIEffect
+    data object NavigateToSignIn : ProfileUIEffect
+
 }
